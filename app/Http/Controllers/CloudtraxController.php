46e5c21ea90df8770d6a1796e363b19974cb6352
jojo-pc <jojo.almostover@gmail.com>
2016-06-16 17:15:09 +0700

    redirect error

--- app/Http/Controllers/CloudtraxController.php
+++ app/Http/Controllers/CloudtraxController.php
@@ -63,7 +63,7 @@ class CloudtraxController extends Controller
             "&password=" . urlencode($encoded_password);
 
         $ads = Session::get('ads');
-        dd($ads);
+        dd($ads['redirect_url']);
         return redirect()->to($ads['redirect_url']);
 
 //            return redirect($redirect_url);
