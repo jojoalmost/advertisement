b5a02bd05c05c5777929500db36e2b39044384dd
jojo-pc <jojo.almostover@gmail.com>
2016-06-17 15:54:17 +0700

    redirect error

--- app/Http/Controllers/CloudtraxController.php
+++ app/Http/Controllers/CloudtraxController.php
@@ -62,13 +62,26 @@ class CloudtraxController extends Controller
             "username=" . urlencode($username) .
             "&password=" . urlencode($encoded_password);
 
-//        return redirect()->to($ads['redirect_url']);
-//            return redirect($redirect_url);
-        $http = curl_init($redirect_url);
-        curl_setopt($http, CURLOPT_RETURNTRANSFER, TRUE);
-        $http_result = curl_exec($http);
-        $http_status = curl_getinfo($http, CURLINFO_HTTP_CODE);
-        curl_close($http);
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, "http://$uamip:$uamport/logon?");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "username=" . urlencode($username) .
+            "&password=" . urlencode($encoded_password));
+
+// in real life you should use something like:
+// curl_setopt($ch, CURLOPT_POSTFIELDS,
+//          http_build_query(array('postvar1' => 'value1')));
+
+// receive server response ...
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $server_output = curl_exec($ch);
+
+        curl_close($ch);
+
+
         $ads = Session::get('ads');
         return redirect($ads['redirect_url']);
     }
