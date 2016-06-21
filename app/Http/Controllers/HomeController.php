d4c261b7822574ff0016b09da5715ea4825150ed
jojo-pc <jojo.almostover@gmail.com>
2016-06-21 19:47:27 +0700

    url portal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -152,7 +152,7 @@ class HomeController extends Controller
                     break;
                 case "notyet":
                     $data = Setting::where('option', 'terms')->firstOrFail();
-                    $portal_mode = Setting::where('option', 'portal_moda')->where('value', 'without')->first();
+                    $portal_mode = Setting::where('option', 'portal_moda')->first();
                     return view('terms-of-use',compact('data','portal_mode'));
                     break;
                 default:
