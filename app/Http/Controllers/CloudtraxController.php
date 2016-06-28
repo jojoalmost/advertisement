0ad4a2b09a6bb825e6017efc32b220942f9b78b4
jojo-pc <jojo.almostover@gmail.com>
2016-06-28 13:24:20 +0700

    loping redirect

--- app/Http/Controllers/CloudtraxController.php
+++ app/Http/Controllers/CloudtraxController.php
@@ -68,22 +68,26 @@ class CloudtraxController extends Controller
 
 //        $redirect_url="http://google.com";
 //        if ($redirect_setting == 'radius') {
-//            return redirect($redirect_url);
+            return redirect($redirect_url);
         }
 //        } elseif ($redirect_setting == 'with') {
-            $http = curl_init();
-            curl_setopt($http, CURLOPT_URL, $redirect_url);
-            curl_setopt($http, CURLOPT_RETURNTRANSFER, TRUE);
-            $data = curl_exec($http);
-            $http_status = curl_getinfo($http, CURLINFO_HTTP_CODE);
-            $err = curl_error($http);
-            curl_close($http);
-            $ads = Session::get('ads');
-            dd($redirect_url,$data,$http_status,$err);
+//            $http = curl_init();
+//            curl_setopt($http, CURLOPT_URL, $redirect_url);
+//            curl_setopt($http, CURLOPT_RETURNTRANSFER, TRUE);
+//            $data = curl_exec($http);
+//            $http_status = curl_getinfo($http, CURLINFO_HTTP_CODE);
+//            $err = curl_error($http);
+//            curl_close($http);
+//            $ads = Session::get('ads');
+//            dd($redirect_url,$data,$http_status,$err);
 //            return redirect()->to($ads['redirect_url']);
 //        }
     }
 
+    public  function redirect(){
+        $ads = Session::get('ads');
+        return redirect()->to($ads['redirect_url']);
+    }
     /**
      * Show the form for creating a new resource.
      *
